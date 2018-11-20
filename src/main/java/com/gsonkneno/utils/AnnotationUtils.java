89ca037efb0037a871f28166adcefde68fedbb20
gaosong <805940564@qq.com>
2018-11-21 00:29:25 +0800

    模仿springboot递归获取注解

---
+++ src/main/java/com/gsonkneno/utils/AnnotationUtils.java
@@ -0,0 +1,36 @@
+
+package com.gsonkneno.utils;
+
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
+/**
+ * 摘抄自springframework
+ */
+public abstract class AnnotationUtils {
+    public static boolean isInJavaLangAnnotationPackage( String annotationType) {
+        return (annotationType != null && annotationType.startsWith("java.lang.annotation"));
+    }
+
+    /**
+     *  模仿自 recursivelyCollectMetaAnnotations.recursivelyCollectMetaAnnotations 方法
+     * 递归获取注解 annotation 上的注解
+     * @param visited
+     * @param annotation
+     */
+    public static void recursivelyCollectMetaAnnotations(Set<Annotation> visited, Annotation annotation) {
+        Class<? extends Annotation> annotationType = annotation.annotationType();
+        String annotationName = annotationType.getName();
+        if (!AnnotationUtils.isInJavaLangAnnotationPackage(annotationName) && visited.add(annotation)) {
+            try {
+                for (Annotation metaMetaAnnotation : annotationType.getAnnotations()) {
+                    recursivelyCollectMetaAnnotations(visited, metaMetaAnnotation);
+                }
+            }
+            catch (Throwable ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
+
--- src/test/java/com/gsonkneno/AppTest.java
+++ src/test/java/com/gsonkneno/AppTest.java
@@ -1,10 +1,15 @@
 package com.gsonkneno;
 
 import com.gsonkneno.annotation.Configuration;
+import com.gsonkneno.annotation.SpringBootConfiguration;
 import com.gsonkneno.bean.BlackPeople;
+import com.gsonkneno.bean.People;
+import com.gsonkneno.utils.AnnotationUtils;
 import org.junit.Test;
 
 import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class AppTest {
@@ -20,6 +25,30 @@ public class AppTest {
             if (annotation instanceof Configuration){
                 System.out.println(((Configuration)annotation).value());
             }
+        }
+    }
+
+    @Test
+    public void test1(){
+        //所有注解类型均继承自 Annotation接口
+        Class<? extends Annotation> annotationClass = SpringBootConfiguration.class;
+
+        //当注解类型不是java.lang.annotation包下的注解类型时
+        if (!AnnotationUtils.isInJavaLangAnnotationPackage(annotationClass.getName())){
+
+            //获取注解类SpringBootConfiguration上的注解
+            Annotation[] annotations = annotationClass.getAnnotations();
+            Set<Annotation> annotationSet = new HashSet<>();
+
+            for (Annotation annotation: annotations) {
+                System.out.println(annotation);
+                AnnotationUtils.recursivelyCollectMetaAnnotations(annotationSet,annotation);
+            }
+
+            //annotationSet 包含SpringBootConfiguration注解上的递归注解
+            System.out.println(annotationSet);
+
+
         }
     }
 
